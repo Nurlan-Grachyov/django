fae15ae8b648eef7e82706b2283dfc429d36adef
Nurlan <nurlan.grachyov@mail.ru>
2024-12-30 22:56:25 +0400

    problems with photo

--- src/connect.py
+++ src/connect.py
@@ -28,7 +28,7 @@ class MyServer(BaseHTTPRequestHandler):
             self.send_header("Content-type", "text/html")
             self.end_headers()
             with open(
-                "C:/Users/Nurlan/IT/Проекты/django/html/catalog.html",
+                "C:/Users/Nurlan/IT/Проекты/django/html/contacts.html",
                 "r",
                 encoding="utf-8",
             ) as file:
@@ -38,7 +38,7 @@ class MyServer(BaseHTTPRequestHandler):
             self.send_header("Content-type", "text/html")
             self.end_headers()
             with open(
-                    "C:/Users/Nurlan/IT/Проекты/django/html/catalog.html",
+                    "C:/Users/Nurlan/IT/Проекты/django/html/contacts.html",
                     "r",
                     encoding="utf-8",
             ) as file:
